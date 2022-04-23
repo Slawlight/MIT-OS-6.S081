a4f84e7466cc1b11f4116232491f22c162185f00
Slawlight <1151628361@qq.com>
2022-04-23 17:22:34 +0800

    5 lock2

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,7 +8,7 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
-pthread_mutex_t lock;
+pthread_mutex_t lock[NBUCKET];
 
 
 struct entry {
@@ -45,7 +45,7 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
-  pthread_mutex_lock(&lock);
+  pthread_mutex_lock(&lock[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -57,7 +57,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-  pthread_mutex_unlock(&lock);   
+  pthread_mutex_unlock(&lock[i]);   
 }
 
 static struct entry*
@@ -107,8 +107,10 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
-
-  pthread_mutex_init(&lock, NULL);
+  for(int i = 0; i < NBUCKET; i++){
+    pthread_mutex_init(&lock[i], NULL);
+  }
+  
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
